3c3923b8a0ff87ce87263f145697342e1ed5ad5c
Jesús <jesusguerreropaniagua6@gmail.com>
2024-10-06 11:32:33 +0200

    feat(ui): :sparkles: added filmName and favourites action to film card

--- packages/ui/src/custom/components/film-card/index.tsx
+++ packages/ui/src/custom/components/film-card/index.tsx
@@ -1,15 +1,26 @@
 import type { Theme } from '@mui/material';
+import Favorite from '@mui/icons-material/Favorite';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 
-import { useTheme } from '../../../core';
+import { Text, useTheme } from '../../../core';
 
 import type { IStyles } from '../../../interfaces/global';
 
 interface IFilmCardProps {
   imgUrl: string;
   onClick: () => void;
+  filmName: string;
+  onClickFavorite?: () => void;
+  isFavorite?: boolean;
 }
+
 const getStyles = (theme: Theme): IStyles => ({
   root: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '1rem',
+  },
+  imgContainer: {
     borderRadius: '32px',
     width: '200px',
     height: 270,
@@ -26,9 +37,25 @@ const getStyles = (theme: Theme): IStyles => ({
     objectFit: 'cover',
     borderRadius: '32px',
   },
+  textContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '0 15px',
+    gap: '10px',
+  },
+  heartIcon: {
+    cursor: 'pointer',
+  },
 });
 
-export default function FilmCard({ imgUrl, onClick }: IFilmCardProps) {
+export default function FilmCard({
+  imgUrl,
+  onClick,
+  filmName,
+  onClickFavorite,
+  isFavorite,
+}: IFilmCardProps) {
   const theme = useTheme();
   const styles = getStyles(theme);
 
@@ -39,8 +66,22 @@ export default function FilmCard({ imgUrl, onClick }: IFilmCardProps) {
   };
 
   return (
-    <div onClick={onClick} onKeyDown={handleKeyPress} style={styles.root}>
-      <img src={imgUrl} alt="film" style={styles.img} />
+    <div style={styles.root}>
+      <div
+        onClick={onClick}
+        onKeyDown={handleKeyPress}
+        style={styles.imgContainer}
+      >
+        <img src={imgUrl} alt="film" style={styles.img} />
+      </div>
+      <div style={styles.textContainer}>
+        <Text text={filmName} variant="caption" />
+        {isFavorite ? (
+          <Favorite onClick={onClickFavorite} style={styles.heartIcon} />
+        ) : (
+          <FavoriteBorder onClick={onClickFavorite} style={styles.heartIcon} />
+        )}
+      </div>
     </div>
   );
 }
